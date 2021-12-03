35250462b4264f31031a90ca65fbefa5bde17cef
Zafar6699 <webzafar6699@gmail.com>
2021-12-03 17:49:27 +0500

    asd

--- controller/Product.js
+++ controller/Product.js
@@ -48,7 +48,7 @@ exports.getOne = async(req, res) => {
 };
 
 exports.getByCategory = async(req, res) => {
-    await Product.find({ category: req.params.id, count: { $gte: 0 } }).exec(
+    await Product.find({ category: req.params.id, count: { $gt: 0 } }).exec(
         (err, data) => {
             if (err) return res.status(400).json({ success: false, err });
             return res.status(200).json({ success: true, data });
